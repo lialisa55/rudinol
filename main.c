698ae1562a784db5a27909901c3658ec6331af9e
Nathalia Monalisa Coelho Mota <nathaliamonalisa150@usp.br>
2024-04-01 10:19:21 -0300

    adicionando programa

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+
+{
+    int Case;
+    char nome1, nome2, Char;
+
+    scanf("%d\n", &Case);
+    scanf("%c\n", &nome1);
+    scanf("%c\n", &nome2);
+    scanf("%c", &Char);
+
+    int resposta;
+    resposta = Case;
+
+    printf("a resposta eh %d", resposta);
+
+    return 0;
+}
